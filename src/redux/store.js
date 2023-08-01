f9601a4bf9136d0a070e479a7fb4f2e0e7d7377f
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-08-02 02:45:21 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {userReducer } from "./reducers/userReducer";
 
-export const server = "http://localhost:5000/api/v1";
+export const server = "https://nice-lime-brown-bear-tam.cyclic.cloud/api/v1";
 
 const store = configureStore({
   reducer: {
